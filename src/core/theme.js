e66a2261c0b826798f583673ea48cded49fd31b5
Oscar Cerda <oskarcr.2019@gmail.com>
2026-04-05 19:13:21 -0600

    Paleta de colores

---
+++ src/core/theme.js
@@ -0,0 +1,72 @@
+const Theme = {
+    /**
+     * Es el color primario de la aplicacion, un tono
+     * oscuro, ideal para botones.
+     * @readonly
+     */
+    PRIMARY: "#754a32",
+
+    /**
+     * Es un color que apya al `PRIMARY`, ideal para botones
+     * extras / tono de ayuda.
+     * @readonly
+     */
+    SECONDARY: "#a07258",
+
+    /**
+     * Es un color para llamar la atencion a pequeños detalles, 
+     * pero sin ser tan intenso como `PRIMARY` o `SECONDARY`.
+     * @readonly
+     */
+    ACCENT: "#d1ac98",
+
+    /**
+     * Es el color negro, simplemente eso.
+     * @readonly
+     */
+    BLACK: "#000000",
+
+    /**
+     * Es una estructura que sirve para guardar colores
+     * relacionados al fondo.
+     * @readonly
+     */
+    BACKGROUND: {
+        /**
+         * Color principal de los fondos.
+         * @readonly
+         */
+        MAIN: "#ffffff",
+
+        /**
+         * Color que sirve como apoyo para no usar `BACKGROUND.MAIN`.
+         * @readonly
+         */
+        SURFACE: "#f0ddd5"
+    },
+
+    /**
+     * Es una estructura que ayuda a guardar colores relacionados a los
+     * textos de la aplicacion.
+     * @readonly
+     */
+    TEXT: {
+        /**
+         * Color oscuro para el texto.
+         * @readonly
+         */
+        PRIMARY: "#000000",
+
+        /**
+         * Color claro para el texto.
+         * @readonly
+         */
+        SECONDARY: "#ffffff"
+    }
+};
+
+Object.freeze(Theme);
+Object.freeze(Theme.BACKGROUND);
+Object.freeze(Theme.TEXT);
+
+export default Theme;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,16 @@
+export default function Login() {
+    return(<>
+        <link rel="stylesheet" href="css/login.css"/>
+        <div>
+            <div className=""></div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkvm2dTG6wPKXPmUamhjI_Q7yq4xpsBGw1DA&s"/>
+            <div>EMPIEZA A DECORAR TU HOGAR</div>
+            <form>
+                <input type="email" placeholder="Correo"/>
+                <input type="password" placeholder="Contraseña"/>
+            </form>
+            <div>¿No tienes cuenta?</div>
+            <div></div>
+        </div>
+    </>);
+}
