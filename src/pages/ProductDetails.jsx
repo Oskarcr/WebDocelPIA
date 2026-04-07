3eb60b77376cc80b93eb13bf8cbc2ced1fd08d3e
RILV <ricardo.lopezvrgs@uanl.edu.mx>
2026-04-06 22:24:16 -0600

    Detalles del producto mejorado, cambios a la pagina inicio y otras cosas

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,29 +3,63 @@ import { Link as RouterLink } from "react-router-dom";
 
 export default function ProductDetails() {
     return (
-        <>
-            <div className="header-wrapper">
-                <div className="title-wrapper">
-                    DoCeL
-                </div>
-                <div className="buttons-wrapper">
-                    <RouterLink to="/" className="link-button">
-                        <Components.ButtonHeader className="header-button" name={"INICIO"}/>
-                    </RouterLink>
-                    <RouterLink to="/not_found" className="link-button">
-                        <Components.ButtonHeader className="header-button" name={"PEDIDOS"}/>
-                    </RouterLink>
-                    <RouterLink to="/shopping" className="link-button">
-                        <Components.ButtonHeader className="header-button" name={"MUEBLES"}/>
-                    </RouterLink>
-                    <RouterLink to="/not_found" className="link-button">
-                        <Components.ButtonHeader className="header-button" name={"PERFIL"}/>
-                    </RouterLink>
+        <Components.Main horizontal>
+            <Components.Column color={Theme.ACCENT} />
+            <div style={{
+                flex: 1,
+                backgroundColor: Theme.BACKGROUND.SURFACE,
+                position: "relative",
+            }}>
+                <Components.DimmedImage src="furniture/background.png" childStyle={{
+                    width: "100%",
+                    height: "100%"
+                }}
+                    style={{
+                        width: "100%",
+                        height: "35%",
+                        top: 0,
+                        left: 0,
+                        position: "absolute",
+                    }}>
+                    <Components.TextBox
+                        content="DETALLES DEL PRODUCTO"
+                        color={Theme.TEXT.SECONDARY}
+                        fontSize="2.5rem"
+                        alignment="center"
+                        style={{
+                            boxSizing: "border-box",
+                            width: "65%",
+                            height: "50%",
+                            marginInline: "auto" 
+                        }}
+                    />
+                </Components.DimmedImage>
+                <div id="product-wrapper" style={{ backgroundColor: Theme.BACKGROUND.MAIN }}>
+                    <div id="product-card" style={{
+                        backgroundColor: Theme.BACKGROUND.MAIN,
+                        borderColor: Theme.PRIMARY
+                        }}>
+                        <div className="product-image">
+                            <img src="furniture/bed_base.png" className="image"></img>
+                        </div>
+                        <div className="product-information">
+                            <div className="product">
+                                <Components.TextBox content="Mueble de madera bonito" color={Theme.PRIMARY} fontSize="28px"/>
+                                <Components.TextBox content="Precio: 800MXN" color={Theme.PRIMARY} fontSize="28px"/>
+                                <Components.TextBox content="Acabado: Laca" color={Theme.PRIMARY} fontSize="28px"/>
+                                <div className="product-color" style={{
+                                    backgroundColor: Theme.BACKGROUND.MAIN,
+                                    color: Theme.PRIMARY
+                                    }}>Color</div>
+                            </div>
+                            <RouterLink>
+                                <button className="add-product">Agregar</button>
+                            </RouterLink>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div id="product-wrapper" style={{backgroundColor: Theme.BACKGROUND.MAIN}}>
-                <div id="product-card"></div>
-            </div>
-        </>
+            <Components.Column color={Theme.BLACK} />
+        </Components.Main>
     )
 }
