8a1424df9ca7c3612e57da60136107bebf528cab
Oscar Cerda <oskarcr.2019@gmail.com>
2026-04-08 23:43:21 -0600

    Un poco de responsividad en el header, y paginas de empleadosy reportes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,30 @@
 import { Components, Theme } from "@/DocelCore";
+import { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
+function ButtonLink({desktop = false, path = "/", name="ANY"}) {
+    if(desktop) {
+        return (<RouterLink to={path} className="link-button" data-device="desktop">
+            <Components.ButtonHeader name={name}/>
+        </RouterLink>);
+    }
+    else {
+        return (<RouterLink to={path} style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "end",
+            width: "100%",
+            height: "100px",
+            fontSize: "1.5rem",
+            color: "var(--primary-color)"
+        }}>
+            {name}
+        </RouterLink>)
+    }
+}
+
 export default function Header({ inverted = false }) {
+    const [menuVisible, setMenuVisible] = useState(false);
     const headerVars = (!inverted ? {} : {
         "--primary-color": Theme.BACKGROUND.MAIN,
         "--text-color-secondary": Theme.PRIMARY
@@ -11,18 +34,36 @@ export default function Header({ inverted = false }) {
             DoCeL
         </div>
         <div className="buttons-wrapper">
-            <RouterLink to="/" className="link-button">
-                <Components.ButtonHeader name={"INICIO"}/>
-            </RouterLink>
-            <RouterLink to="/orders" className="link-button">
-                <Components.ButtonHeader name={"PEDIDOS"}/>
-            </RouterLink>
-            <RouterLink to="/shopping" className="link-button">
-                <Components.ButtonHeader name={"MUEBLES"}/>
-            </RouterLink>
-            <RouterLink to="/profile" className="link-button">
-                <Components.ButtonHeader name={"PERFIL"}/>
-            </RouterLink>
+            <ButtonLink path="/" name="INICIO" desktop/>
+            <ButtonLink path="/orders" name="PEDIDOS" desktop/>
+            <ButtonLink path="/shopping" name="MUEBLES" desktop/>
+            <ButtonLink path="/profile" name="PERFIL" desktop/>
+
+            <div data-device="mobile" style={{
+                display: "flex",
+                flexDirection: "row"
+            }} onClick={() => setMenuVisible(!menuVisible)}>
+                <Components.ButtonHeader name="MENU" />
+                {menuVisible && (
+                    <div style={{
+                        padding: "20px",
+                        boxSizing: "border-box",
+                        top: "100%",
+                        right: "0",
+                        position: "absolute",
+                        display: "flex",
+                        flexDirection: "column",
+                        width: "100vw",
+                        backgroundColor: "var(--text-color-secondary)",
+                        boxShadow: "var(--box-shadow)"
+                    }}>
+                        <ButtonLink path="/" name="INICIO"/>
+                        <ButtonLink path="/orders" name="PEDIDOS"/>
+                        <ButtonLink path="/shopping" name="MUEBLES"/>
+                        <ButtonLink path="/profile" name="PERFIL"/>
+                    </div>
+                )}
+            </div>
         </div>
     </div>);
 }
---
+++ src/components/Table.jsx
@@ -0,0 +1,98 @@
+import { Theme } from "@/DocelCore";
+import { useNavigate } from "react-router-dom";
+
+
+/**
+ * @typedef TableParams
+ * @property {JSX.Element[]} head
+ * @property {JSX.Element[][]} elements
+ * @property {import("react").CSSProperties} style
+ * @property {import("react").CSSProperties} headStyle
+ * @property {import("react").CSSProperties} elementsStyle
+ * @property {string} size
+ * @property {string} fontSize 
+ * @property {string} color
+ * @property {string} backgroundColor
+ */
+
+/**
+ * @param {TableParams} param0 
+ */
+export default function Table({
+    color = Theme.PRIMARY,
+    size = "var(--box-size)",
+    fontSize = "1.1rem",
+    backgroundColor = Theme.BACKGROUND.SURFACE,
+    rowActiveColor = Theme.ACCENT,
+    head = ["Test", "Test2"],
+    elements = [["body1", "body2"], [null, "body3"]],
+    style = {},
+    headStyle = {},
+    elementsStyle = {}
+}) {
+const navigate = useNavigate();
+    const headElements = [];
+    const hlen = head.length;
+
+    for (let i = 0; i < hlen; i++) {
+        headElements.push(<th style={{
+            padding: "12px",
+            outline: "1px solid" + color,
+            height: size,
+            ...headStyle
+        }}>
+            {(head[i] ?? head[i])}
+        </th>);
+    }
+
+    const bodyElements = [];
+
+    for (let i = 0; i < elements.length; i++) {
+        if (!Array.isArray(elements[i])) elements[i] = [];
+        bodyElements[i] = [];
+        for (let j = 0; j < hlen; j++) {
+            bodyElements[i].push(<td style={{
+                padding: "12px",
+                border: "5px solid " + Theme.PRIMARY,
+                height: size,
+                ...elementsStyle
+            }}>
+                {elements[i][j] ?? elements[i][j]}
+            </td>);
+        }
+    }
+
+    return (<div style={{
+        position: "relative",
+        overflowY: "auto",
+        boxSizing: "border-box",
+        outline: "inset 3px solid" + Theme.PRIMARY,
+        boxShadow: "var(--box-shadow)",
+        "--tr-active-color": rowActiveColor,
+        ...style
+    }} onClick={() => {navigate("/sales")}}>
+        <table style={{
+            borderCollapse: "collapse",
+            backgroundColor: backgroundColor,
+            tableLayout: "fixed",
+            fontSize: fontSize,
+            width: "100%",
+        }}>
+            <thead style={{
+                outline: "3px solid" + Theme.PRIMARY,
+                backgroundColor: color
+            }}>   
+                <tr>
+                    {headElements}
+                </tr>
+            </thead>
+            <tbody style={{
+                color: color
+            }}>
+                {bodyElements.map(a => 
+                <tr style={{ cursor: "pointer" }}>{a}</tr>
+                )}
+            </tbody>
+        </table>
+    </div>);
+}
