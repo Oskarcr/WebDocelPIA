908a0ef031c0378ab49402588b7cc6cecbe0e419
RILV <ricardo.lopezvrgs@uanl.edu.mx>
2026-04-09 17:29:44 -0600

    Cambios

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,65 +1,26 @@
 import { Components, FontSize, Theme } from "@/DocelCore";
 import { Link as RouterLink } from "react-router-dom";
+import Flex from "../components/Flex";
 
 export default function ProductDetails() {
     return (
         <Components.Main horizontal>
             <Components.Column color={Theme.ACCENT} />
-            <div style={{
+            <div className="product-wrapper" style={{
+                display: "flex",
                 flex: 1,
-                backgroundColor: Theme.BACKGROUND.SURFACE,
-                position: "relative",
+                background: "red"
             }}>
-                <Components.DimmedImage src="furniture/background_product.png" childStyle={{
-                    width: "100%",
-                    height: "100%"
-                }}
-                    style={{
-                        width: "100%",
-                        height: "35%",
-                        top: 0,
-                        left: 0,
-                        position: "absolute",
-                    }}>
-                    <Components.TextBox
-                        content="DETALLES DEL PRODUCTO"
-                        color={Theme.TEXT.SECONDARY}
-                        fontSize={FontSize.XL1}
-                        alignment="center"
-                        style={{
-                            boxSizing: "border-box",
-                            width: "65%",
-                            height: "50%",
-                            marginInline: "auto"
-                        }}
-                    />
-                </Components.DimmedImage>
-                <div id="product-wrapper">
-                    <div id="product-card" style={{
-                        backgroundColor: Theme.BACKGROUND.MAIN,
-                        borderRadius: "8px",
-                        overflow: "hidden",
-                        borderColor: Theme.PRIMARY
-                    }}>
-                        <div className="product-image" style={{borderRadius: "8px"}}>
-                            <img src="furniture/bed_base.png" className="image"></img>
-                        </div>
-                        <div className="product-information">
-                            <div className="product">
-                                <Components.TextBox content="Mueble de madera bonito" color={Theme.PRIMARY} fontSize="28px" />
-                                <Components.TextBox content="Precio: 800MXN" color={Theme.PRIMARY} fontSize="28px" />
-                                <Components.TextBox content="Acabado: Laca" color={Theme.PRIMARY} fontSize="28px" />
-                                <div className="product-color" style={{
-                                    backgroundColor: Theme.BACKGROUND.MAIN,
-                                    color: Theme.PRIMARY
-                                }}>Color</div>
-                            </div>
-                            <RouterLink>
-                                <button className="add-product">Agregar</button>
-                            </RouterLink>
-                        </div>
-                    </div>
-                </div>
+                <div className="product-card" style={{
+                    display: "flex",
+                    aspectRatio: "16/9",
+                    minHeight: "200px",
+                    width: "60%",
+                    margin: "auto",
+                    backgroundColor: "var(--primary-color)",
+                    boxSizing: "border-box",
+                    border: "var(--border-medium) solid"
+                }}></div>
             </div>
             <Components.Column color={Theme.BLACK} />
         </Components.Main>
