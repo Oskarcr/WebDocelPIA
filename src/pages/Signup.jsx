4628f82fb18ce8c4c21b5965ae5d2b372838d3fa
RILV <ricardo.lopezvrgs@uanl.edu.mx>
2026-05-13 15:51:10 -0600

    Avance de signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,34 @@
 import { Components, FontSize, Spacing, Theme } from "@/DocelClient";
 
+async function handleSubmit(evt) {
+    evt.preventDefault();
+
+    const formData = new FormData(evt.target);
+
+    const body = {
+        name: formData.get("name"),
+        phone: formData.get("phone"),
+        address: formData.get("address"),
+        email: formData.get("email"),
+        password: formData.get("password")
+    }
+
+    const response = await fetch("auth/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+
+    const data = await response.json();
+
+    console.log(data);
+}
+
 export default function Signup() {
     return (<Components.Main horizontal>
-        <Components.Column color={Theme.BLACK}/>
+        <Components.Column color={Theme.BLACK} />
         <Components.Flex row style={{
             backgroundColor: Theme.BACKGROUND.MAIN
         }}>
@@ -21,40 +47,48 @@ export default function Signup() {
                     gap: Spacing.MD,
                     overflow: "auto"
                 }}>
-                    <Components.TextBox 
+                    <Components.TextBox
                         fontSize={FontSize.MD}
                         content="REGISTRARSE EN DOCEL"
                     />
-                    <input type="text" placeholder="Nombre"/>
-                    <input type="number" placeholder="Telefono"/>
-                    <input type="text" placeholder="Domicilio"/>
-                    <input type="email" placeholder="Correo"/>
-                    <input type="password" placeholder="Contraseña"/>
-                    <button style={{
-                        marginTop: Spacing.MD
-                    }}>REGISTRARME</button>
-                    <Components.TextBox 
-                        content="Ya tengo cuenta" 
+                    <form onSubmit={handleSubmit} style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        width: "100%",
+                        gap: Spacing.MD,
+                        
+                    }}>
+                        <input type="text" name="name" placeholder="Nombre" />
+                        <input type="number" name="phone" placeholder="Telefono" />
+                        <input type="text" name="address" placeholder="Domicilio" />
+                        <input type="email" name="email" placeholder="Correo" />
+                        <input type="password" name="password" placeholder="Contraseña" />
+                        <button type="submit" style={{
+                            marginTop: Spacing.MD
+                        }}>REGISTRARME</button>
+                    </form>
+                    <Components.TextBox
+                        content="Ya tengo cuenta"
                         alignment="center"
                         href="/login"
                     />
                 </div>
-                <Components.Column color={Theme.ACCENT} horizontal/>
+                <Components.Column color={Theme.ACCENT} horizontal />
             </Components.Flex>
-            
-            <Components.DimmedImage 
-            device="desktop"
-            orientation="portrait" style={{
-                height: "100%",
-                width: "50%",
-                padding: Spacing.MD,
-                boxSizing: "border-box"
-            }} childStyle={{
-                position: "relative",
-                display: "flex",
-                flexDirection: "column",
-                height: "100%",
-            }} src="https://www.shutterstock.com/image-photo/senior-carpenter-guides-woman-using-600nw-2585917355.jpg">
+
+            <Components.DimmedImage
+                device="desktop"
+                orientation="portrait" style={{
+                    height: "100%",
+                    width: "50%",
+                    padding: Spacing.MD,
+                    boxSizing: "border-box"
+                }} childStyle={{
+                    position: "relative",
+                    display: "flex",
+                    flexDirection: "column",
+                    height: "100%",
+                }} src="https://www.shutterstock.com/image-photo/senior-carpenter-guides-woman-using-600nw-2585917355.jpg">
                 <Components.TextBox
                     style={{
                         marginTop: "auto"
@@ -63,7 +97,7 @@ export default function Signup() {
                     fontSize={FontSize.XL3}
                     content="COMIENZA\nCON\nNOSOTROS"
                 />
-                <Components.TextBox 
+                <Components.TextBox
                     style={{
                         marginTop: "auto"
                     }}
@@ -74,6 +108,6 @@ export default function Signup() {
                 />
             </Components.DimmedImage>
         </Components.Flex>
-        <Components.Column color={Theme.ACCENT}/>
+        <Components.Column color={Theme.ACCENT} />
     </Components.Main>);
 }
